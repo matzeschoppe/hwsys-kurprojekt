632e453aa049d90f6b341bd9311b144a35e81442
matzeschoppe <64011560+matzeschoppe@users.noreply.github.com>
2023-06-26 20:45:30 +0200

    WIP - algo testbench

--- algorithmus_tb.vhdl
+++ algorithmus_tb.vhdl
@@ -19,6 +19,39 @@ architecture TESTBENCH of ALGORITHMUS_TB is
             p2_btn_left_in:  	in    std_logic;
             p2_btn_right_in:  	in    std_logic; 
             start_btn_in:  		in    std_logic;
-            );
-     end component;
+        );
+    end component;
 
+    --Clock period
+    constant period: time:= 25 ms; --25khz Clock frequency
+
+    -- Signals
+    signal clk, reset std_logic := '0';
+    signal p1_btn_left_in, p1_btn_right_in, p2_btn_left_in, p2_btn_right_in, start_btn_in std_logic :='0';
+    signal leds_out: std_logic_vector (5 downto 0);
+
+    begin
+        -- port map
+        IMPL: ALGORITHMUS port map( clk => clk, reset => reset, leds_out => leds_out, 
+                                    p1_btn_left_in => p1_btn_left_in, p1_btn_right_in => p1_btn_right_in,
+                                    p2_btn_left_in => p2_btn_left_in, p2_btn_right_in => p2_btn_right_in, 
+                                    start_btn_in => start_btn_in);
+        process
+
+        -- helper to perform clock cycle
+        procedure run_cycle is 
+            begin 
+                clk <= '0';
+            wait for period/2;
+                clk <= '1';
+            wait for period/2;
+        end procedure;
+
+        begin
+            start_btn_in <= '1';
+            -- run enough cycle for blinking to be over
+            -- test what happens on button press during wait
+            -- test what happens
+
+        end process;
+end TESTBENCH;
